6761f5ae3ca08dc3d33333c019094501887f0510
Keoslsc <38363461+Keoslsc@users.noreply.github.com>
2019-03-05 00:29:59 -0600

    Update DatabaseSeeder.php
    Error in email virified

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $user->email = 'administrator@administrator.com';
         $user->student_code = 123456789;
         $user->password = bcrypt('udg123');
-        $user->email_verify_at = '2019-03-05 06:01:06';
+        $user->email_verified_at = '2019-03-05 06:01:06';
         $user->is_social_service = 1;
         $user->role = 'admin';
         $user->save();
